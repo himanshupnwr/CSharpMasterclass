27ca0bafe15c15e1e7bc1f7a3d95c2924a5d5e70
Himanshu Panwar <Himanshu_Panwar@Dell.com>
2022-05-09 17:27:06 +0530

    Add project files.

---
+++ CSharpMasterclass/DataTypes/HelloWorld.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpMasterclass.DataTypes
+{
+    internal class HelloWorld
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 13;
+            int num2 = 5;
+            int sum = num1 + num2;
+
+            double d1 = 3.5;
+            double d2 = 1.337;
+            double sumD = d1 / d2;
+
+            Console.WriteLine("The sum of " + num1 + " and " + num2 + " is " + sum);
+            Console.WriteLine(d1 + " devided by " + num2 + " is " + sumD);
+            Console.Read();
+        }
+    }
+}
